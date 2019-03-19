e6de2b63db86512299b3411215b2a34d3b26d2f0
Jennifer Perez <jenniferperez@pursuit.org>
2019-03-19 01:06:38 -0400

    text box

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+  state={
+    input:'',
+    display:''
+  }
+
+textBox=(e)=>{
+    this.setState({input:e.target.value})
+}
+
+buttonSubmitted = (e) => {
+  // console.log("clicked")
+  this.setState({display:this.state.input})
+}
+
   render() {
+    console.log(this.state)
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+   <>
+   <input type='text'onChange={this.textBox}/>
+   <button type='submit' onClick={this.buttonSubmitted}></button>
+   <h1>{this.state.display}</h1>
+   </>
     );
   }
 }
 
 export default App;
+
+
+
