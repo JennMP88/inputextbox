a39183f8b710fe7c3cd2de2196352992fe1afc49
Jennifer Perez <jenniferperez@pursuit.org>
2019-03-19 13:19:37 -0400

    todo list

--- src/App.js
+++ src/App.js
@@ -1,35 +1,80 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
-class App extends Component {
-  state={
-    input:'',
-    display:''
-  }
+class ToDo extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            items: [],
+            input: '',
+            currentItem: ''
+        }
+    }
+  
 
-textBox=(e)=>{
-    this.setState({input:e.target.value})
+    clickingItem = (e, idx) => {
+        // console.dir(e.target)
+        const currentItem = this.state.items[idx]
+
+        let array = [...this.state.items]; 
+        let index = array.indexOf(currentItem)
+        console.log(idx)
+        if (idx !== -1) {
+            array.splice(idx, 1);
+            this.setState({ items: array });
+    }}
+
+
+
+onItems = (e) => {
+    console.log(e.target.value)
+    this.setState({ input: e.target.value })
 }
 
-buttonSubmitted = (e) => {
-  // console.log("clicked")
-  this.setState({display:this.state.input})
-}
-
-  render() {
-    console.log(this.state)
+addItems = (e) => {
     
-    return (
-   <>
-   <input type='text'onChange={this.textBox}/>
-   <button type='submit' onClick={this.buttonSubmitted}></button>
-   <h1>{this.state.display}</h1>
-   </>
-    );
-  }
+    let copiedItems = [...this.state.items]
+
+    copiedItems.push(this.state.input)
+
+    this.setState({ items: copiedItems })
+
 }
 
-export default App;
 
+render() {
+    console.log(this.state)
+    return (
+        <>
+            <div className='row'>
+                <div className='col-6'>
+                    <div><h3>To do List</h3></div>
+                    <input type='text' placeholder='todo' onChange={this.onItems} /><button onClick={this.addItems}>add</button>
+
+                </div>
+
+                <div className='col-6'>
+                    <div><h3> List</h3></div>
+                    <ul class="list-group">
+                        {
+                            this.state.items.map((items, i) => {
+                                let activeFeed = " ";
+                                if (items === this.state.currentItem) activeFeed = "active"
+                                return <li key={i} className={"list-group-item d-flex justify-content-between align-items-center"} >{items}
+                                    <span class={"badge badge-primary badge-pill " + activeFeed} onClick={(e) => this.clickingItem(e, i)} >x</span></li>
+                            })
+                        }
+                    </ul>
+                </div>
+            </div>
+            
+
+
+        </>
+    )
+}
+
+}
+export default ToDo;
 
 
